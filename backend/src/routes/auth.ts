c6a93a8a4c923234213d04239bab6dcc9c345b9f
yashveeMavani <117334937+yashveeMavani@users.noreply.github.com>
2024-08-12 05:15:06 +0000

    added project files

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,84 @@
+import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import verifyToken from "../middleware/auth";
+
+const router = express.Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "1d" }
+      );
+
+      // res.cookie("auth_token", token, {
+      //   httpOnly: true,
+      //   maxAge: 86400000,
+      //   path: "/",
+      // });
+
+      // Set the cookie to be persistent even after window reload
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        maxAge: 86400000,
+        path: "/",
+        sameSite: "none",
+        secure: true,
+      });
+      res.status(200).json({ userId: user._id });
+      console.log("User logged in");
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+  res.status(200).send({ userId: req.userId });
+});
+
+router.post("/logout", (req: Request, res: Response) => {
+  // res.cookie("auth_token", "", {
+  //   expires: new Date(0),
+  // });
+  res.cookie("auth_token", "", {
+    httpOnly: true,
+    maxAge: 0,
+    path: "/",
+    sameSite: "none",
+    secure: true,
+  });
+  res.send();
+});
+
+export default router;
